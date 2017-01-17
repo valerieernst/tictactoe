f33e0a2ab716e73a2b9d895fa77f30319bd73151
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-01-17 14:20:49 -0800

    Add jquery to allow user to interact with board

--- client/tictactoe.js
+++ client/tictactoe.js
@@ -1,1 +1,26 @@
-console.log('hi!')
+$(document).ready(function(){
+  let playerOneTurn = true;
+
+  if(playerOneTurn) {
+    $('#turnLabel').text("Player One's Turn");
+  } else {
+    $('#turnLabel').text("Player Two's Turn");
+  }
+
+
+//toggles peices on board
+  $("td").click(function(){
+    if(playerOneTurn) {
+      $(this).toggleClass('selectedX');
+      playerOneTurn = !playerOneTurn;
+    } else {
+      $(this).toggleClass('selectedO');
+      playerOneTurn = !playerOneTurn;
+    }
+  });
+
+
+
+
+
+});
