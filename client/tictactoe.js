66becee897cbab411d0151e3d31746cd2e4b379f
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-01-17 14:33:26 -0800

    Add ability to clear board

--- client/tictactoe.js
+++ client/tictactoe.js
@@ -1,24 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function() {
   let playerOneTurn = true;
 
-  if(playerOneTurn) {
-    $('#turnLabel').text("Player One's Turn");
-  } else {
-    $('#turnLabel').text("Player Two's Turn");
-  }
-
-
 //toggles peices on board
-  $("td").click(function(){
+  $("td").click(function() {
     if(playerOneTurn) {
       $(this).toggleClass('selectedX');
+
+      $('#turnLabel').text("Player Two's Turn");
       playerOneTurn = !playerOneTurn;
     } else {
       $(this).toggleClass('selectedO');
+      $('#turnLabel').text("Player One's Turn");
       playerOneTurn = !playerOneTurn;
     }
   });
 
+  $('button').click(function() {
+    $('td').removeClass();
+    $('td').addClass('noneSelected');
+    console.log('this works')
+  })
 
 
 
