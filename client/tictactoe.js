04c63d15aecd306911ab61256e6c8136a43aff91
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-01-17 14:39:18 -0800

    Added explaination of unfinished problem

--- client/tictactoe.js
+++ client/tictactoe.js
@@ -1,27 +1,37 @@
 $(document).ready(function() {
   let playerOneTurn = true;
 
-//toggles peices on board
+//when user clicks, toogle peices on board and check to see if there is a winner
   $("td").click(function() {
     if(playerOneTurn) {
       $(this).toggleClass('selectedX');
-
       $('#turnLabel').text("Player Two's Turn");
+      lookForWinner();
       playerOneTurn = !playerOneTurn;
     } else {
       $(this).toggleClass('selectedO');
       $('#turnLabel').text("Player One's Turn");
+      lookForWinner();
       playerOneTurn = !playerOneTurn;
     }
   });
 
   $('button').click(function() {
-    $('td').removeClass();
-    $('td').addClass('noneSelected');
-    console.log('this works')
+    clearBoard()
   })
 
+  function clearBoard() {
+    console.log('in here')
+    $('td').removeClass();
+    $('td').addClass('noneSelected');
+  }
 
+  function lookForWinner(player) {
+    //want to iterate through tic tac toe board and look to see if player who just went has won
+    //if player won, 
+      //announce that player has won
+      //clear board using clearBoard function
+  }
 
 
 });
